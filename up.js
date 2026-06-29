ce7fd5575b011abe71f8145230f040d36948b2a7
Nikita <boreyko1901@mail.ru>
2026-06-29 23:31:54 +0300

    Update up.js

--- up.js
+++ up.js
@@ -20,3 +20,12 @@ const observer = new IntersectionObserver((entries) => {
 document.querySelectorAll(".fade-in").forEach(el=>{
     observer.observe(el);
 });
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-7QYMNWVD9P"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-7QYMNWVD9P');
+</script>
